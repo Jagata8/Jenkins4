2e45e8aede52b293cd2d3464d41583c83b248583
Jagata8 <134514761+Jagata8@users.noreply.github.com>
2024-02-06 15:24:45 -0500

    Jenkins4.groovy

--- Jenkins4.groovy
+++ Jenkins4.groovy
@@ -1,6 +1,8 @@
 pipeline {
     agent any
+    
     tools {
+        // Define tools to be used in the pipeline
         jdk 'jdk11'
         maven 'maven'
     }
@@ -8,16 +10,23 @@ pipeline {
     stages {
         stage('Git Checkout') {
             steps {
+                // Checkout code from the specified Git repository
                 git branch: 'main', url: 'https://github.com/Jagata8/Task4.git'
             }
         }
+        
         stage('Code Compile') {
             steps {
+                // Compile the code using Maven
                 bat label: 'Compile', script: 'mvn clean compile'
             }
         }
+        
         stage('Unit Tests') {
             steps {
+                // Run unit tests using Maven
                 bat label: 'Run Tests', script: 'mvn test'
             }
         }
+    }
+}
